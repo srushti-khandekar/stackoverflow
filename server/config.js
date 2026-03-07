fda0e506decaacb9dab3e78dca9d1160c8b79ed9
srushti-khandekar <srushti.khandekar@agivant.com>
2026-03-07 13:40:25 +0000

    change db

--- server/config.js
+++ server/config.js
@@ -1,7 +1,27 @@
+// module.exports = {
+//   port: process.env.PORT || 8080,
+//   db: {
+//     prod: process.env.DATABASE_URL || 'mongodb://15.207.141.133/stackoverflow-clone',
+//     test: 'mongodb://localhost/stackoverflow-test',
+//     options: {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true
+//     }
+//   },
+//   jwt: {
+//     secret: process.env.JWT_SECRET || 'development_secret',
+//     expiry: '7d'
+//   }
+// };
+
 module.exports = {
   port: process.env.PORT || 8080,
   db: {
-    prod: process.env.DATABASE_URL || 'mongodb://15.207.141.133/stackoverflow-clone',
+    // CHANGE THIS LINE:
+    prod: process.env.DATABASE_URL || 'mongodb://mongo:27017/stackoverflow-clone',
+    
     test: 'mongodb://localhost/stackoverflow-test',
     options: {
       useNewUrlParser: true,
